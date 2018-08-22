328edc87c5b77650a9ddb3be9035ac881dc79529
Unknown <luizfernandovieira_cf@hotmail.com>
2018-08-22 17:07:08 -0300

    add listagem de copas

--- src/app/copas/copas.component.ts
+++ src/app/copas/copas.component.ts
@@ -9,12 +9,20 @@ import { Copa } from '../copa';
 })
 export class CopasComponent implements OnInit {  
 
+  /*
   copa: Copa = {
     ano: 1930,
     dataInicio: new Date(1930, 7, 13),
     dataFim: new Date(1930, 7, 30),
     nomeCidadeAbertura: 'Montevidéu'
   };
+  */
+
+  copas: Copa[] = [
+    { ano: 1930, dataInicio: new Date(1930, 7, 13), dataFim: new Date(1930, 7, 30), nomeCidadeAbertura: 'Montevidéu' },
+    { ano: 1934, dataInicio: new Date(1934, 5, 27), dataFim: new Date(1934, 6, 10), nomeCidadeAbertura: 'Roma' },
+    { ano: 1938, dataInicio: new Date(1938, 6, 4), dataFim: new Date(1938, 6, 19), nomeCidadeAbertura: 'Paris' }
+  ];
 
   constructor() { }
 
